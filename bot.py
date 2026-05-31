cb7d493e957acb651c2982b8017d690ab8f82953
AVLOR7 <xxwafi45@gmail.com>
2026-05-31 08:56:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,13 @@ async def button_click(update, context):
     
     try:
         url = query.data
-        ydl_opts = {'outtmpl': 'video.mp4', 'format': 'best', 'quiet': True}
+        ydl_opts = {
+    'outtmpl': 'video.mp4',
+    'format': 'best',
+    'quiet': True,
+    'no_warnings': True,
+    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
         
